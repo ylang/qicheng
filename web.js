707653a0a3a9be73956b4b49ffe773226823dd15
ylang <ylang@andrew.cmu.edu>
2013-12-27 14:44:58 -0800

    init with node + soy

--- web.js
+++ web.js
@@ -1,21 +1,51 @@
 var path = require('path');
 var express = require('express');
 var http = require('http');
+var os = require('os');
+var soynode = require('soynode');
+
+soynode.setOptions({
+    outputDir: os.tmpdir(),
+    uniqueDir: true,
+    allowDynamicRecompile: true,
+    eraseTemporaryFiles: true
+});
+
 console.log("hello");
 function init() {
     var app = express();
     configureExpress(app);
-    http.createServer(app).listen(process.env.PORT || 8000, function() {
-        console.log("Express server listening on port %d", process.env.PORT || 8000);
+    require('./routes/index')(app);
+    soynode.compileTemplates('views', function(err) {
+        if (err) throw err;
+        http.createServer(app).listen(app.get('port'), function(){
+            console.log('Express server listening on port ' + app.get('port'));
+        });
     });
-
-
 }
 
 init();
 
 function configureExpress(app) {
     app.configure(function() {
+        app.set('port', process.env.PORT || 8000);
+        app.set('view engine', ".soy");
+
+        var soyRenderer = function(_path, options, callback) {
+            var templatePath =
+                    _path.replace(path.normalize(this.root + '/'), '');
+            templatePath =
+                    templatePath.replace('.soy',path.sep + options['function']);
+            templatePath = templatePath.split(path.sep).join('.');
+            callback(null, options.soynode.render(templatePath, options));
+        };
+
+        app.engine('.soy', soyRenderer);
+        app.use(function(req, res, next) {
+            res.locals.soynode = soynode;
+            next();
+        });
+
         app.use(express.bodyParser());
         app.use(express.methodOverride());
 
